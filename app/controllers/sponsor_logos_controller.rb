5bbe0d23bd4b989bc9ea80036176044c8caaf537
Mattycodes <matt.nick.fagioli@gmail.com>
2018-03-12 21:45:06 -0400

    Adds logic for sponsor logos.

--- app/controllers/sponsor_logos_controller.rb
+++ app/controllers/sponsor_logos_controller.rb
@@ -6,25 +6,40 @@ class SponsorLogosController < ApplicationController
   def create
     @logo = SponsorLogo.new(logo_params)
     if @logo.present? && @logo.save
-      redirect_to root_path
+      redirect_to sponsor_logo_path(@logo)
     else
       @error_message = "Could not save logo, please fill all fields correctly."
       render 'new' and return
     end
   end
 
+  def show
+    find_logo_by_id_or_redirect
+  end
+
   def destroy
+    find_logo_by_id_or_redirect
+    @logo.destroy
+    redirect_to root_path
   end
 
   def edit
+    find_logo_by_id_or_redirect
   end
 
   def update
+    find_logo_by_id_or_redirect
+    redirect_to sponsor_logo_path(@logo) if @logo.update_attributes(logo_params)
   end
 
   private
 
   def logo_params
     params.require(:sponsor_logo).permit(:name, :logo)
+  end
+
+  def find_logo_by_id_or_redirect
+    @logo = SponsorLogo.where(id: params[:id]).first
+    redirect_to root_path if !@logo.present?
   end
 end
