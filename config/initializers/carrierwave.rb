ec60a4ec59b69401cd4db9c82515d21663d8999b
Mattycodes <matt.nick.fagioli@gmail.com>
2018-03-25 18:37:33 -0400

    implements fog storage for prod.

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,23 @@
+CarrierWave.configure do |config|
+  if Rails.env.development? || Rails.env.test?
+    CarrierWave.configure do |config|
+      config.storage = :file
+    end
+  end
+
+  if Rails.env.production?
+    CarrierWave.configure do |config|
+      config.storage = :fog
+    end
+  end
+
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+    :region                 => 'eu-west-1'
+  }
+
+  config.fog_directory  = 'xplodethis'
+  config.fog_attributes = { 'Cache-Control'=>'max-age=315576000' }
+end
