4e59bd2439ed814b03ac22f87815a55df609f0e2
Mattycodes <matt.nick.fagioli@gmail.com>
2018-03-25 17:48:23 -0400

    Tries smtp method.

--- config/initializers/mailer_config.rb
+++ config/initializers/mailer_config.rb
@@ -1,11 +1,21 @@
-ActionMailer::Base.delivery_method = :sendmail
-ActionMailer::Base.sendmail_settings = {
-  :address => ENV['FROM_ADDRESS'],
-  :port => "587",
-  :domain => "gmail.com",
-  :host => ENV['HOST_NAME'],
-  :user_name => ENV['FROM_ADDRESS'],
-  :password => ENV['FROM_ADDRESS_PASSWORD'],
-  :authentication => "plain",
-  :enable_starttls_auto => true
+# ActionMailer::Base.delivery_method = :sendmail
+# ActionMailer::Base.sendmail_settings = {
+#   :address => ENV['FROM_ADDRESS'],
+#   :port => "587",
+#   :domain => "gmail.com",
+#   :host => ENV['HOST_NAME'],
+#   :user_name => ENV['FROM_ADDRESS'],
+#   :password => ENV['FROM_ADDRESS_PASSWORD'],
+#   :authentication => "plain",
+#   :enable_starttls_auto => true
+# }
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+     :address => "smtp.gmail.com",
+     :port => 587,
+     :user_name => ENV['FROM_ADDRESS'],
+     :password => ENV['FROM_ADDRESS_PASSWORD'],
+     :authentication => :plain,
+     :enable_starttls_auto => true
 }
