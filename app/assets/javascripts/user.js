4f02bf16f16a6a7ca037c8328be8a089f897ae8f
Mattycodes <matt.nick.fagioli@gmail.com>
2018-03-25 10:22:45 -0400

    Finishes about page with percentage bars.

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,31 @@
+document.addEventListener("turbolinks:load", function() {
+  skillbarListener();
+});
+
+function loadSkillbars() {
+  var $skillbars = $(".skillbar-filler");
+
+  $skillbars.map(function(skillbar, index) {
+    if ( isScrolledIntoView( $(this) ) ) {
+      var width = ( $(this).data("percent") - 5 ) + "%";
+      $(this).animate({ width: width }, 2500);
+    }
+  });
+};
+
+function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ( (elemBottom <= docViewBottom) && (elemTop >= docViewTop) );
+};
+
+function skillbarListener() {
+  loadSkillbars();
+
+  $( document ).scroll(function() {
+    loadSkillbars();
+  })
+};
