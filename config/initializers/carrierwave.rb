db7b88cc9d171ad7eeac3aef799bc8f79e2653f0
Mattycodes <matt.nick.fagioli@gmail.com>
2018-03-25 18:45:09 -0400

    Moves config methods around

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,18 +7,18 @@ CarrierWave.configure do |config|
     end
   end
 
+  if Rails.env.production?
+    CarrierWave.configure do |config|
+      config.storage = :fog
+    end
+  end
+
   config.fog_credentials = {
     :provider               => 'AWS',
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
     :region                 => 'eu-west-1'
   }
-
-  if Rails.env.production?
-    CarrierWave.configure do |config|
-      config.storage = :fog
-    end
-  end
 
   config.fog_directory  = 'xplodethis'
   config.fog_attributes = { 'Cache-Control'=>'max-age=315576000' }
