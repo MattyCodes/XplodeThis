fe27e370c59d31b0fbeec3eec8e09ed52b969499
Mattycodes <matt.nick.fagioli@gmail.com>
2018-03-25 21:29:17 -0400

    tries including bucket name in uploader paths.

--- app/uploaders/header_uploader.rb
+++ app/uploaders/header_uploader.rb
@@ -1,6 +1,6 @@
 class HeaderUploader < CarrierWave::Uploader::Base
   def store_dir
-    "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "xplodethis/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def extension_whitelist
