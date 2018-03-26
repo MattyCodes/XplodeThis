22b12cb96683b02bc6a3b8dd2d4ca785f88abb94
Mattycodes <matt.nick.fagioli@gmail.com>
2018-03-25 21:23:50 -0400

    Specifies bucket name again.

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,6 +1,6 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def default_url(*args)
--- app/uploaders/header_uploader.rb
+++ app/uploaders/header_uploader.rb
@@ -1,6 +1,6 @@
 class HeaderUploader < CarrierWave::Uploader::Base
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def extension_whitelist
