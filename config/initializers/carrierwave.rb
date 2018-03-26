7055b7907a68866c1440eb93c90120cbde6c9d1c
Mattycodes <matt.nick.fagioli@gmail.com>
2018-03-25 21:42:46 -0400

    Fixes conditional (again).

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,18 +7,19 @@ if Rails.env.development? || Rails.env.test?
 else
   CarrierWave.configure do |config|
     config.storage = :fog
+
+
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region                 => 'eu-west-1',
+      :endpoint => "https://s3.eu-west-1.amazonaws.com/xplodethis/",
+      :path_style             => true
+    }
+
+    config.storage        = :fog
+    config.fog_directory  = 'xplodethis'
+    config.fog_attributes = { 'Cache-Control'=>'max-age=315576000' }
   end
-
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-    :region                 => 'eu-west-1',
-    :endpoint => "https://s3.eu-west-1.amazonaws.com/xplodethis/",
-    :path_style             => true
-  }
-
-  config.storage        = :fog
-  config.fog_directory  = 'xplodethis'
-  config.fog_attributes = { 'Cache-Control'=>'max-age=315576000' }
 end
