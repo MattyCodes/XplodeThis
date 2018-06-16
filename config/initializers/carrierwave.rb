9a7baa525ca4e5c4d123d7c5f5b20b628af10e95
Mattycodes <matt.nick.fagioli@gmail.com>
2018-06-16 16:20:11 -0400

    Changes AWS storage region.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,8 +11,7 @@ else
       :provider               => 'AWS',
       :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
       :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region                 => 'eu-west-1',
-      :endpoint => "https://s3.eu-west-1.amazonaws.com",
+      :region                 => 'us-east-2',
       :path_style             => true
     }
 
